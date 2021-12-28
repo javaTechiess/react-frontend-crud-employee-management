e66d0c86d2edd577f07d0385e4581ea07a266c9d
javaTechiess <javatechiess@gmail.com>
2021-12-28 15:47:50 +0530

    first commit

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class FooterComponent extends Component {
+    constructor(props) {
+        // Pass props to the super class.
+        super(props)
+
+        this.state = {
+
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+                <footer className="footer">
+                   
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default FooterComponent;
